87cc59c94c96ecca5564ed9a6d3750a49c66a223
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-05-09 13:39:50 +0200

    functor implementation

--- src/series1.ml
+++ src/series1.ml
@@ -10,38 +10,8 @@ open Bigarray
 (* common types, e.g., for row data *)
 type data_type = DFloat of float | DInt of int | DStr of string
 
+
 module type S = sig
-  type t
-  (** Abstract data type for series of data *)
-
-  type el_t
-  (** The data type of a single element *)
-
-  val name : t -> string
-  (** The name identifier for the data column *)
-
-  val from_list : name:string -> el_t list -> t
-  (** [from_list ~name l] constructs a data series instance from
-      given list [l] and assigns [name] to it. *)
-
-  val from_strings : name:string -> string list -> t
-  (** [from_string strs name] contructs a data series from the
-      given list of strings [strs] *)
-
-  val get : int -> t -> data_type
-  (** [get i d] returns the element of [d] at index [i].
-      Throws out-of-bounds exception, if [i] is out-of-bounds. *)
-
-  val set : int -> data_type -> t -> unit
-  (** [set i d s] sets the element at index [i] to [d] of series [s].
-      Throws out-of-bounds exception, if [i] is out-of-bounds. *)
-
-  val length : t -> int
-  (** [length d] returns the length (= number of entries) of the data series [s]. *)
-end
-
-
-module type S1 = sig
   type t
   (** Abstract data type for series of data *)
 
@@ -76,7 +46,7 @@ module Numeric = struct
     val dtype_kind : (dtype, dtype_elt) kind
   end
 
-  module Make (N : Num) : (S1 with type dtype := N.dtype) = struct
+  module Make (N : Num) : (S with type dtype := N.dtype) = struct
   type t = { name : string; data : (N.dtype, N.dtype_elt, c_layout) Array1.t }
 
   let name s = s.name
@@ -93,88 +63,48 @@ module Numeric = struct
 end
 
 
-(*
-module type S_el = sig
-  type t
-  (** Abstract data type for series of data *)
+module Generic = struct
+  module type Element = sig
+    type dtype
+  end
 
-  type el_t
-  (** The data type of a single element *)
-
-  val from_string : string -> t option
-  (** [from_string s] converts given string [s] to the
-      desired (numerical) type. Returns [None], if convesion fails *)
-
-end
-*)
-
-(* Float series *)
-module Floats : S with type el_t := float = struct
-  type t = { name : string; data : (float, float64_elt, c_layout) Array1.t }
-
-  let name s = s.name
-
-  let from_list ~name l =
-    { name; data = Array1.of_array float64 c_layout (Array.of_list l) }
-
-  let from_strings ~name _strs =
-    let _ = name in
-    failwith "Not implemented"
-
-  let get idx s = DFloat (Array1.get s.data idx)
-
-  let set idx d s =
-    match d with
-    | DFloat d -> Array1.set s.data idx d
-    | _ -> failwith "Invalid data type"
-
-  let length d = Array1.dim d.data
-end
-
-(* Int series *)
-module Ints : S with type el_t := int = struct
-  type t = { name : string; data : (int, int_elt, c_layout) Array1.t }
-
-  let name s = s.name
-
-  let from_list ~name l =
-    { name; data = Array1.of_array int c_layout (Array.of_list l) }
-
-  let from_strings ~name _strs =
-    let _ = name in
-    failwith "Not implemented"
-
-  let get idx s = DInt (Array1.get s.data idx)
-
-  let set idx d s =
-    match d with
-    | DInt d -> Array1.set s.data idx d
-    | _ -> failwith "Invalid data type"
-
-  let length d = Array1.dim d.data
-end
-
-(* String series *)
-module Strings : S with type el_t := string = struct
-  type t = { name : string; data : string array }
+  module Make (E : Element) : (S with type dtype := E.dtype) = struct
+  type t = { name : string; data : E.dtype array }
 
   let name s = s.name
 
   let from_list ~name l = { name; data = Array.of_list l }
 
-  let from_strings ~name _strs =
-    let _ = name in
-    failwith "Not implemented"
+  let get idx s = s.data.(idx)
 
-  let get idx s = DStr s.data.(idx)
-
-  let set idx d s =
-    match d with
-    | DStr d -> s.data.(idx) <- d
-    | _ -> failwith "Invalid data type"
+  let set idx d s = s.data.(idx) <- d
 
   let length d = Array.length d.data
+
+  end
+
 end
+
+
+(* Float series *)
+module Floats = Numeric.Make(struct
+    type dtype = float
+    type dtype_elt = float64_elt
+    let dtype_kind = float64
+  end)
+
+(* Int series *)
+module Ints = Numeric.Make(struct
+    type dtype = int
+    type dtype_elt = int_elt
+    let dtype_kind = int
+  end)
+
+(* String series *)
+module Strings = Generic.Make(struct
+    type dtype = string
+  end)
+
 
 (* basic module type for data series *)
 (* FIXME: maybe a GADT could help to reduce boilerplate code here *)
@@ -186,14 +116,15 @@ let name = function
   | SStr s -> Strings.name s
 
 let get idx = function
-  | SFloat s -> Floats.get idx s
-  | SInt s -> Ints.get idx s
-  | SStr s -> Strings.get idx s
+  | SFloat s -> DFloat (Floats.get idx s)
+  | SInt s -> DInt (Ints.get idx s)
+  | SStr s -> DStr (Strings.get idx s)
 
-let set idx d = function
+(*let set idx d = function
   | SFloat s -> Floats.set idx d s
   | SInt s -> Ints.set idx d s
-  | SStr s -> Strings.set idx d s
+  | SStr s -> Strings.set idx d s *)
+let set _idx _d _s = failwith "Not implemented"
 
 type summary = { name : string; data_type : string; length : int }
 
--- src/series1.mli
+++ src/series1.mli
@@ -14,52 +14,14 @@
 type data_type =
   | DFloat of float
   | DInt of int
-  | DStr of string  (** Data type of a single element of a data series *)
+  | DStr of string
+  (** Data type of a single element of a data series *)
 
 type summary = { name : string; data_type : string; length : int }
 (** Some key properties of a data series, like its name, type, and length *)
 
+
 module type S = sig
-  type t
-  (** Abstract data type for series of data *)
-
-  type el_t
-  (** The data type of a single element *)
-
-  val name : t -> string
-  (** The name identifier for the data column *)
-
-  val from_list : name:string -> el_t list -> t
-  (** [from_list ~name l] constructs a data series instance from
-      given list [l] and assigns [name] to it. *)
-
-  val from_strings : name:string -> string list -> t
-  (** [from_string strs name] contructs a data series from the
-      given list of strings [strs] *)
-
-  val get : int -> t -> data_type
-  (** [get i d] returns the element of [d] at index [i].
-      Throws out-of-bounds exception, if [i] is out-of-bounds. *)
-
-  val set : int -> data_type -> t -> unit
-  (** [set i d s] sets the element at index [i] to [d] of series [s].
-      Throws out-of-bounds exception, if [i] is out-of-bounds. *)
-
-  val length : t -> int
-  (** [length d] returns the length (= number of entries) of the data series [s]. *)
-end
-
-module Floats : S with type el_t := float
-(** Module Floats for series of float data *)
-
-module Ints : S with type el_t := int
-(** Module Ints for series of integer data *)
-
-module Strings : S with type el_t := string
-(** Module Strings for series of string data *)
-
-
-module type S1 = sig
   type t
   (** Abstract data type for series of data *)
 
@@ -84,6 +46,16 @@ module type S1 = sig
   val length : t -> int
   (** [length d] returns the length (= number of entries) of the data series [s]. *)
 end
+
+
+module Floats : S with type dtype := float
+(** Module Floats for series of float data *)
+
+module Ints : S with type dtype := int
+(** Module Ints for series of integer data *)
+
+module Strings : S with type dtype := string
+(** Module Strings for series of string data *)
 
 
 type t =
