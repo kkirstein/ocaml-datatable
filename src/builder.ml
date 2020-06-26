d3856fdf74291644c68615c3795e0fcb4f7a7c68
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-06-26 14:07:00 +0200

    implement builder functor

--- src/builder.ml
+++ src/builder.ml
@@ -32,7 +32,12 @@ module type S = sig
 
   val add : row_t -> t -> t
 
-  val to_table : t -> Table.t
+  val to_table :
+    t ->
+    ( Table.t,
+      [> `Invalid_column | `Invalid_datatype | `Invalid_index | `Invalid_length ]
+    )
+    result
 end
 
 (** Functor to generate a data table builder *)
@@ -48,5 +53,47 @@ module Make (Data : D) : S with type row_t := Data.rowdata = struct
   let add data builder =
     { builder with rows = Data.convert data :: builder.rows }
 
-  let to_table _builder = failwith "Not implemented"
+  let create_cols builder dt =
+    let open Series in
+    let cols = Table.Row.bindings builder.colspec in
+    let len = List.length builder.rows in
+    let rec loop cols res =
+      match res with
+      | Ok dt -> (
+          match cols with
+          | (n, d) :: cs -> (
+              match d with
+              | `Float ->
+                  loop cs
+                    (Table.add_col
+                       (SFloat (Floats.create ~name:n ~value:0.0 len))
+                       dt)
+              | `Int ->
+                  loop cs
+                    (Table.add_col (SInt (Ints.create ~name:n ~value:0 len)) dt)
+              | `String ->
+                  loop cs
+                    (Table.add_col
+                       (SStr (Strings.create ~name:n ~value:"" len))
+                       dt) )
+          | [] -> res )
+      | Error _ as e -> e
+    in
+    loop cols (Ok dt)
+
+  let fill_cols builder dt =
+    match
+      List.fold_left
+        (fun res r ->
+          match res with
+          | Ok n -> Table.set_row r (n + 1) dt
+          | Error _ as e -> e)
+        (Ok (-1)) builder.rows
+    with
+    | Ok _ -> Ok dt
+    | Error _ as e -> e
+
+  let to_table builder =
+    let ( >>= ) = Result.bind in
+    Table.empty builder.name |> create_cols builder >>= fill_cols builder
 end
