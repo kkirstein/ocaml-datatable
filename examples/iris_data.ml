a415acb1cd85ec1dd8b04ccce2709db7ba79a491
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-06-29 09:00:03 +0200

    use builder API

--- examples/iris_data.ml
+++ examples/iris_data.ml
@@ -11,9 +11,7 @@
 
 open Datatable
 
-let parse_float str =
-  match float_of_string_opt str with Some f -> f | None -> Float.nan
-
+(* column specification *)
 let col_spec =
   let open Table in
   Row.empty
@@ -22,6 +20,10 @@ let col_spec =
   |> Row.add "petal_length" `Float
   |> Row.add "petal_width" `Float
   |> Row.add "species" `String
+
+(* convert single record *)
+let parse_float str =
+  match float_of_string_opt str with Some f -> f | None -> Float.nan
 
 let row_from_strings strs =
   let open Table in
@@ -36,62 +38,25 @@ let row_from_strings strs =
       |> Row.add "species" (DStr spec)
   | _ -> Row.empty
 
+
+module Csv_reader = Builder.Make (struct
+  type record = string list
+  let convert = row_from_strings
+end)
+
 (* read data from CSV file *)
 let read_data path =
   let ci = open_in path in
   let csv = Csv.of_channel ~has_header:true ci in
-  let data =
-    Csv.fold_left ~f:(fun acc r -> row_from_strings r :: acc) ~init:[] csv
-    |> List.fold_left
-         (fun acc x -> if Table.Row.is_empty x then acc else x :: acc)
-         []
+  let table =
+    Csv.fold_left
+      ~f:(fun reader r -> Csv_reader.add r reader)
+      ~init:(Csv_reader.create "iris" col_spec)
+      csv
   in
   Csv.close_in csv;
-  data
+  Csv_reader.to_table table
 
-(* convert row list to table *)
-let create_cols col_spec len dt =
-  let open Series in
-  let cols = Table.Row.bindings col_spec in
-  let rec loop cols res =
-    match res with
-    | Ok dt -> (
-        match cols with
-        | (n, d) :: cs -> (
-            match d with
-            | `Float ->
-                loop cs
-                  (Table.add_col
-                     (SFloat (Floats.create ~name:n ~value:0.0 len))
-                     dt)
-            | `Int ->
-                loop cs
-                  (Table.add_col (SInt (Ints.create ~name:n ~value:0 len)) dt)
-            | `String ->
-                loop cs
-                  (Table.add_col
-                     (SStr (Strings.create ~name:n ~value:"" len))
-                     dt) )
-        | [] -> res )
-    | Error _ as e -> e
-  in
-  loop cols (Ok dt)
-
-let fill_cols row_data dt =
-  match
-    List.fold_left
-      (fun res r ->
-        match res with Ok n -> Table.set_row r (n + 1) dt | Error _ as e -> e)
-      (Ok (-1)) row_data
-  with
-  | Ok _ -> Ok dt
-  | Error _ as e -> e
-
-let from_row_list name rows =
-  let ( >>= ) = Result.bind in
-  let dt = Table.empty name in
-  let len = List.length rows in
-  if len > 0 then create_cols col_spec len dt >>= fill_cols rows else Ok dt
 
 let summary_to_string sum =
   let open Table in
@@ -101,12 +66,7 @@ let summary_to_string sum =
 let () =
   let tic = Sys.time () in
   Printf.printf "Loading data from CVS ..";
-  let data = read_data "./examples/iris.csv" in
+  let dt = read_data "./examples/iris.csv" |> Result.get_ok in
   let toc = Sys.time () in
-  Printf.printf " done (%d records in %.3fs).\n" (List.length data) (toc -. tic);
-  let tic = Sys.time () in
-  Printf.printf "Importing data ..";
-  let dt = from_row_list "iris" data |> Result.get_ok in
-  let toc = Sys.time () in
-  Printf.printf " done (%d rows in %.3fs):\n" (Table.length dt) (toc -. tic);
+  Printf.printf " done (%d records in %.3fs).\n" (Table.length dt) (toc -. tic);
   Printf.printf "%s\n" (Table.summary dt |> summary_to_string)
